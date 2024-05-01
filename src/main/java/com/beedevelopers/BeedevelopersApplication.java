72bbaee2b63c890ccfddf50ea89506ea2e8fd085
Jair0305 <chamaco_03@hotmail.es>
2024-05-01 10:16:48 -0600

    setup spring boot project

---
+++ src/main/java/com/beedevelopers/BeedevelopersApplication.java
@@ -0,0 +1,13 @@
+package com.beedevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeedevelopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeedevelopersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beedevelopers/BeedevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beedevelopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeedevelopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
